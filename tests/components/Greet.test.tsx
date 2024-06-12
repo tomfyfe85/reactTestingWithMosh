448ce18b03a689d74387c0cf6c94ec2a1ef9acfc
Tom Fyfe <tomfyfe85@gmail.com>
2024-06-12 11:14:32 +0100

    simplifying test setup complete

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,8 +1,8 @@
-import { it, expect, describe } from "vitest";
-import { render, screen } from "@testing-library/react";
+// import { it, expect, describe } from "vitest";
+import { render, screen } from '@testing-library/react'
 import Greet from "../../src/components/Greet";
-import "@testing-library/jest-dom/vitest";
-
+// import "@testing-library/jest-dom/vitest";
+// commented out because global settings for vitest are enabled- see setup.ts/vitest.config.ts/tsconfig.ts
 describe("Greet", () => {
   it("should render Hello with the name when the name is provided", () => {
     render(<Greet name="Tom" />);
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    globals: true,
+    // allows global settings - so I can remove imports for vitest in every file
+    // "types": ["vitest/globals"] in tsconfig.json/"compilerOptions"
+    setupFiles: 'tests/setup.ts'
   }
-});
+});  
